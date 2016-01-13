968a4c2d26640ed8d87c31d4c0d7f708470b7781
Matt Molnar <mmolnar@rootinc.com>
2016-01-13 09:54:12 -0500

    Cache false wasn't working

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 $(async function(){
-  const imgSmall = 'testdata/flower-500k.jpg';
-  const imgBig = 'testdata/road-3600k.jpg';
+  const imgSmall = 'http://rootinc.github.io/images/flower-500k.jpg';
+  const imgBig = 'http://rootinc.github.io/images/road-3600k.jpg';
 
   const
     txtBox = $('#text'),
@@ -19,17 +19,20 @@ $(async function(){
   let logSeqTotalEnd = logStart('sequential total');
 
   let logEnd1 = logStart(`sequential ${imgSmall}`);
-  await $.get(imgSmall, {cache:false});
+  await $.get({url:imgSmall, cache:false});
   logEnd1();
 
   let logEnd2 = logStart(`sequential ${imgBig}`);
-  await $.get(imgBig, {cache:false});
+  await $.get({url:imgBig, cache:false});
   logEnd2();
 
   logSeqTotalEnd();
 
   //concurrent
   let logConcTotalEnd = logStart('concurrent total');
-  await* [$.get(imgSmall, {cache:false}), await $.get(imgBig, {cache:false})];
+  await* [
+    $.get({url:imgSmall, cache:false}),
+    $.get({url:imgBig, cache:false})
+  ];
   logConcTotalEnd();
 });
